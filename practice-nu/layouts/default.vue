f00c52319d1b7fbe646b0c5dde9d7d6cb7360ec5
umechazuke <108936354+umechazuke@users.noreply.github.com>
2024-01-29 06:24:37 +0000

    feat: sidebar

--- practice-nu/layouts/default.vue
+++ practice-nu/layouts/default.vue
@@ -1,31 +1,22 @@
 <template>
-    <v-app id="inspire">
-        <v-navigation-drawer v-model="drawer">
-            <!--  -->
-        </v-navigation-drawer>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      permanent
+      class="pt-4"
+      color="grey-lighten-3"
+      rail
+    >
+      <v-avatar
+        v-for="n in 6"
+        :key="n"
+        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
+        :size="n === 1 ? 36 : 20"
+        class="d-block text-center mx-auto mb-9"
+      ></v-avatar>
+    </v-navigation-drawer>
 
-        <v-app-bar>
-            <v-app-bar-nav-icon @click="drawer = !drawer"><v-icon>mdi-home</v-icon></v-app-bar-nav-icon>
-
-            <v-app-bar-title>Application</v-app-bar-title>
-        </v-app-bar>
-
-        <v-main>
-            <nuxt />
-        </v-main>
-    </v-app>
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-app>
 </template>
-
-<script setup>
-import { ref } from 'vue'
-
-const drawer = ref(null)
-</script>
-
-<script>
-export default {
-    data: () => ({
-        drawer: null,
-    }),
-}
-</script>
